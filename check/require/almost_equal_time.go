c1e9b00247521b047dd4f43219843d80495996d1
nikita <dayzwaitik123@yandex.ru>
2026-02-22 17:31:46 +0300

    documentaion for functions

--- check/require/almost_equal_time.go
+++ check/require/almost_equal_time.go
@@ -7,6 +7,8 @@ import (
 	"github.com/sariya23/probatigo/internal"
 )
 
+// AlmostEqualTime is a function that compares two time values, time1 and time2,
+// with a specified precision. This function is Fail Now.
 func AlmostEqualTime(t testing.TB, time1, time2 time.Time, precision time.Duration) {
 	isAlmostEqual := internal.AlmostEqualTime(time1, time2, precision)
 	if isAlmostEqual {
