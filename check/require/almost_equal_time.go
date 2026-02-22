b1cd6c5f47681df32874f676d9463fcea433812f
nikita <dayzwaitik123@yandex.ru>
2026-02-22 17:29:28 +0300

    AlmostEqualTime checkers

--- check/require/almost_equal_time.go
+++ check/require/almost_equal_time.go
@@ -1,4 +1,4 @@
-package assert
+package require
 
 import (
 	"testing"
@@ -10,7 +10,7 @@ import (
 func AlmostEqualTime(t testing.TB, time1, time2 time.Time, precision time.Duration) {
 	isAlmostEqual := internal.AlmostEqualTime(time1, time2, precision)
 	if isAlmostEqual {
-		t.Errorf("time1 is '%v' and time2 is '%v' is not equal with precision '%v'",
+		t.Fatalf("time1 is '%v' and time2 is '%v' is not equal with precision '%v'",
 			time1, time2, precision)
 	}
 }
